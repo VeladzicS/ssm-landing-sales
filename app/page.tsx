e51283d9e706ec4935a8154da848ef32bef07b6d
Senahid Velazic <veladzicsenahid@gmail.com>
2025-08-17 17:17:44 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Hero from "@/components/common/hero";
 import About from "@/components/common/about";
-import Gallery from "@/components/common/gallery";
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
-      <Gallery />
     </>
   );
 }
